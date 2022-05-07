194a8a8eb64ba12fc8a38cd212ccfb20b8945036
Julio Muller <juliodolago@gmail.com>
2022-05-07 00:17:27 -0300

    feat: add and configure email sending

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 
 import { prisma } from './prisma';
 
 const PORT = process.env.PORT || 8082;
 const app = express();
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '7e3086996be235',
+    pass: 'ebb4a7f7093560',
+  },
+});
 
 app.use(express.json());
 
@@ -17,6 +26,38 @@ app.post('/feedbacks', async (req, res) => {
   });
 
   res.status(201).json(feedback);
+  transport.sendMail({
+    from: 'Feedback <noreply@feedget.com>',
+    to: 'Suporte <support@feedget.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<h1 style="font-family: sans-serif;">🚀 Novo feedback</h1>`,
+      '<table style="border-spacing: 20px 8px; font-family: sans-serif; text-align: left;">',
+      '<tr>',
+      '<th>ID</th>',
+      `<td>${feedback.id}</td>`,
+      '</tr>',
+      '<tr>',
+      '<th>Tipo de feedback</th>',
+      `<td>${feedback.type}</td>`,
+      '</tr>',
+      '<tr>',
+      '<th>Comentário</th>',
+      `<td>${feedback.comment}</td>`,
+      '</tr>',
+      ...[
+        feedback.screenshot
+          ? [
+              '<tr>',
+              '<th>Screenshot</th>',
+              `<td><img src="${feedback.screenshot}" /></td>`,
+              '</tr>',
+            ]
+          : [],
+      ],
+      '</table>',
+    ].join(''),
+  });
 });
 
 app.listen(PORT, () => {
