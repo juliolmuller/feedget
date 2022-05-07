f3a6ee77b1d80656d1d2dfc4fe9aa42d8911431c
Julio Muller <juliodolago@gmail.com>
2022-05-06 23:23:27 -0300

    feat: create endpoint to save new feedback

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,23 @@
 import express from 'express';
 
+import { prisma } from './prisma';
+
 const PORT = process.env.PORT || 8082;
 const app = express();
+
+app.use(express.json());
+
+app.post('/feedbacks', async (req, res) => {
+  const feedback = await prisma.feedback.create({
+    data: {
+      type: req.body.type,
+      comment: req.body.comment,
+      screenshot: req.body.screenshot,
+    },
+  });
+
+  res.status(201).json(feedback);
+});
 
 app.listen(PORT, () => {
   console.info(`Server running on http://localhost:${PORT}`);
