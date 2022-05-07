eaed1294977c37f8da9b87948cdca2ef68e27374
Julio Muller <juliodolago@gmail.com>
2022-05-06 22:35:29 -0300

    chore: configure linting tool

---
+++ server/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: [],
+  ignorePatterns: [],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import-helpers', 'prettier'],
+  rules: {
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        alphabetize: {
+          order: 'asc',
+          ignoreCase: true,
+        },
+        groups: ['module', '/^~//', ['parent', 'sibling', 'index']],
+        newlinesBetween: 'always',
+      },
+    ],
+    'prettier/prettier': [
+      'warn',
+      {
+        arrowParens: 'always',
+        printWidth: 80,
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+  },
+};
